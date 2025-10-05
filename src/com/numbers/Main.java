8e4299e7f372784c07f538b3969df5a61e07c780
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-10-05 02:26:29 +0200

    Add files via upload
    Commiting Main class

---
+++ src/com/numbers/Main.java
@@ -0,0 +1,42 @@
+// Java program to find numbers dividable by 3
+
+package com.numbers;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// Main class
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Creating collection to store numbers
+        Queue <Integer> queue = new LinkedList<>();
+
+        // Adding numbers from 0 to 99 to a queue
+        for (int index = 0; index < 100; index++) {
+            queue.add(index);
+        }
+
+        // Printing out numbers with result
+        // Creating iterator
+        Iterator it = queue.iterator();
+
+        // Condition that next value exists
+        while (it.hasNext()) {
+
+            // Creating helping value
+            int tmp = (int)it.next();
+
+            // Printing result to console
+            // Condition of division by 3
+            if (tmp % 3 == 0) {
+                System.out.println(tmp + " is dividable by 3.");
+            } else {
+                System.out.println(tmp + " is not dividable by 3.");
+            }
+        }
+    }
+}
